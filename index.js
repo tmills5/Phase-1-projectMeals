ef9d6369f0b00f9d3a00e0dbacc52b72b1053077
Merle Tyson Mills <tysonmills5@gmail.com>
2021-09-14 08:59:06 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// User should be able to search recipes and see a grid of pictures
+//when click on the pic, the recipe should come up
+//maybe favorite the best recipes??
+//gonna have to create a card for each recipe-iterate through the array 
+
+//---------CONSTANTS!!!------------//
+const listAllMealCategories = "www.themealdb.com/api/json/v1/1/categories.php"
+const searchByFirstLetter = "www.themealdb.com/api/json/v1/1/search.php?f="
+const searchByName = "www.themealdb.com/api/json/v1/1/search.php?s="
+const searchByRandom = "www.themealdb.com/api/json/v1/1/random.php"
+
+//---------EVENT HANDLERS / FETCH HANDLERS---------------//
+
+//---------RENDER FUNCTIONS---------------------//
+
+//---------EVENT LISTENERS-----------------------//
