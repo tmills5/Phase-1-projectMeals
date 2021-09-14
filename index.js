0a00c1295cc4650760056c0494ac837b3ee29566
Merle Tyson Mills <tysonmills5@gmail.com>
2021-09-14 12:12:34 -0400

    adding categories

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-// User should be able to search recipes and see a grid of pictures
+// User should be able to search recipes and see a grid (or list? we'll see) of pictures
 //when click on the pic, the recipe should come up
 //maybe favorite the best recipes??
 //gonna have to create a card for each recipe-iterate through the array 
 
 //---------CONSTANTS!!!------------//
-const listAllMealCategories = "www.themealdb.com/api/json/v1/1/categories.php"
-const listAllByArea = "www.themealdb.com/api/json/v1/1/list.php?a=list"
-const listAllByIngredient = "www.themealdb.com/api/json/v1/1/list.php?i=list"
+const listAllMealCategories = "https://www.themealdb.com/api/json/v1/1/categories.php"
+const listAllByArea = "https://www.themealdb.com/api/json/v1/1/list.php?a=list"
+const listAllByIngredient = "https://www.themealdb.com/api/json/v1/1/list.php?i=list"
 
-const searchByFirstLetter = "www.themealdb.com/api/json/v1/1/search.php?f="
-const searchByName = "www.themealdb.com/api/json/v1/1/search.php?s="
-const searchByRandom = "www.themealdb.com/api/json/v1/1/random.php"
+const searchByName = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+const searchByFirstLetter = "https://www.themealdb.com/api/json/v1/1/search.php?f="
+const searchByRandom = "https://www.themealdb.com/api/json/v1/1/random.php"
+
+const categoriesAllBtn = document.getElementById("categories-all-list")
+const areaAllBtn = document.getElementById("area-all-list")
+const ingredientsAllBtn = document.getElementById("ingredients-all-list")
+const randomAllBtn = document.getElementById("random-list")
 
 //---------EVENT HANDLERS / FETCH HANDLERS---------------//
+const fetchAllCategories = () => {
+//get request to the categories list url and render to page
+    fetch(listAllMealCategories)
+        .then(response => response.json())
+        .then(data => { console.log(data)//---works!
+            mealCategory = data.categories //search within the obj-not sure why? though this was array of obj
+            mealCategory.forEach(elem => {
+                console.log(elem)
+            })
+        })
+}
 
 //---------RENDER FUNCTIONS---------------------//
 
+
 //---------EVENT LISTENERS-----------------------//
+categoriesAllBtn.addEventListener("click", fetchAllCategories)
