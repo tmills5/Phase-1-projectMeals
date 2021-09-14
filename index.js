adc0081af849e3259b6651e790a8cfe4b80c4400
Merle Tyson Mills <tysonmills5@gmail.com>
2021-09-14 14:45:05 -0400

    added category list items

--- index.js
+++ index.js
@@ -17,21 +17,31 @@ const areaAllBtn = document.getElementById("area-all-list")
 const ingredientsAllBtn = document.getElementById("ingredients-all-list")
 const randomAllBtn = document.getElementById("random-list")
 
+const categoryItem = document.getElementById("list-item")
+
 //---------EVENT HANDLERS / FETCH HANDLERS---------------//
 const fetchAllCategories = () => {
 //get request to the categories list url and render to page
     fetch(listAllMealCategories)
         .then(response => response.json())
         .then(data => { console.log(data)//---works!
-            mealCategory = data.categories //search within the obj-not sure why? though this was array of obj
-            mealCategory.forEach(elem => {
-                console.log(elem)
+            mealCategory = data.categories //pull from within the obj-not sure why? though this was array of obj
+            mealCategory.forEach(category => {
+                //console.log(category)
+                renderListItem(category);
             })
         })
 }
 
 //---------RENDER FUNCTIONS---------------------//
-
+const renderListItem = category => {
+    const {strCategory, strCategoryDescription} = category;
+    const categoryLi = document.createElement("li")
+    categoryLi.className = "categoryLi"
+    categoryLi.innerText = `${strCategory}`
+    categoryItem.append(categoryLi)
+}
 
 //---------EVENT LISTENERS-----------------------//
+
 categoriesAllBtn.addEventListener("click", fetchAllCategories)
