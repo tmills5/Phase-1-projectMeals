e122952c7405fd44ae108caaeccd670f099dc068
Merle Tyson Mills <tysonmills5@gmail.com>
2021-09-15 14:40:10 -0400

    search forms almost done

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-// User should be able to search recipes and see a grid (or list? we'll see) of pictures
-//when click on the pic, the recipe should come up
+//instead of listing-consider searching for area, name, and starting letter
 //maybe favorite the best recipes??
-//gonna have to create a card for each recipe-iterate through the array 
+//gonna have to create a card or div for each recipe-iterate through the array 
 
 //---------CONSTANTS!!!------------//
+
+
+
 const listAllMealCategories = "https://www.themealdb.com/api/json/v1/1/categories.php"
 const listAllByArea = "https://www.themealdb.com/api/json/v1/1/list.php?a=list"
 const listAllByIngredient = "https://www.themealdb.com/api/json/v1/1/list.php?i=list"
 
-const searchByName = "https://www.themealdb.com/api/json/v1/1/search.php?s="
-const searchByFirstLetter = "https://www.themealdb.com/api/json/v1/1/search.php?f="
+const searchByNameUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+const searchByFirstLetterUrl = "https://www.themealdb.com/api/json/v1/1/search.php?f="
 const searchByRandom = "https://www.themealdb.com/api/json/v1/1/random.php"
 const singleRandom = "https://www.themealdb.com/api/json/v1/1/random.php"
 
 const filterByCategory = "https://www.themealdb.com/api/json/v1/1/filter.php?c="
+const filterByArea = "http://www.themealdb.com/api/json/v1/1/filter.php?a="
 
 const categoriesAllBtn = document.getElementById("categories-all-list")
 const areaAllBtn = document.getElementById("area-all-list")
@@ -23,7 +26,21 @@ const randomAllBtn = document.getElementById("random-list")
 const categoryItem = document.getElementById("list-item")
 const categoryProfile = document.getElementById("meal-profile")
 
+const searchByname = document.getElementById("search-form-name")
+const searchByFirstLetter = document.getElementById("search-form-letter")
+
+const filterByIngredient = document.getElementById('filter-form-ingredient')
+
+
+
+
+
+
+
 //---------EVENT HANDLERS / FETCH HANDLERS---------------//
+
+
+
 
 const fetchAllCategories = () => {
     //get request to the categories list url and render to page
@@ -44,7 +61,7 @@ const fetchRandom = () => {
     fetch(searchByRandom)
         .then(response => response.json())
         .then(data => {
-            console.log(data)//---works!
+            // console.log(data)//---works!
             mealInfo = data.meals
             mealInfo.forEach(meal => {
                 //console.log(meal)
@@ -52,6 +69,71 @@ const fetchRandom = () => {
             })
         })
 }
+
+const fetchAllAreas = () => {
+    resetPage()
+    fetch(listAllByArea)
+        .then(response => response.json())
+        .then(areas => {
+            //console.log(areas) //------works!!
+            areaInfo = areas.meals
+            areaInfo.forEach(area => { //console.log(area)//--------works
+                renderAreaList(area)
+            })
+        })
+}
+
+// const fetchFilterArea = () => {
+//     fetch(filterByArea)
+//     .then(response => response.json())
+//     .then(results => { console.log(results)
+//         results = results.meals
+//         results.forEach(meal => {
+//             renderRandom(meal)
+//         })
+//     })
+// }
+
+const handleSearchNameResults = (e) => {
+    resetPage()
+    
+    //debugger
+    e.preventDefault()
+    const term = e.target[0].value
+    //can also use below instead of e.target.value
+    //const term = e.target.querySelector('input[name="search-name"]').value
+    
+    
+    //fetch by the term entered by user
+    fetch(searchByNameUrl + term)
+        .then(response => response.json())
+        .then(results => { //console.log(results) //--------works!!
+            results = results.meals
+            results.forEach(result => {
+                renderRandom(result)
+            })
+            e.target[0].value = ""  //resets the search form-coudn't figure out .reset() <--logs "term not a fxn"
+        })
+}
+
+const handleSearchByFirstLetterResults = (e) => {
+    resetPage()
+
+    //debugger
+    e.preventDefault()
+    const term = e.target.querySelector("input[name='search-name']").value
+    fetch(searchByFirstLetterUrl + term)
+        .then(response => response.json())
+        .then(results => { //console.log(results)
+            results = results.meals
+            results.forEach(result => {
+                renderRandom(result)
+            })
+            e.target.querySelector("input[name='search-name']").value = ""
+        })
+}
+
+
 //---------RENDER FUNCTIONS---------------------//
 
 const renderListItem = category => {
@@ -63,7 +145,7 @@ const renderListItem = category => {
 
     categoryLi.addEventListener('click', function () {
         resetPage()
-        const { strCategoryDescription, strCategoryThumb } = category
+        const {strCategoryDescription, strCategoryThumb} = category
         const categoryDiv = document.createElement("div")
         categoryDiv.innerHTML = `
     <h3>${strCategoryDescription}</h3>
@@ -80,23 +162,56 @@ const renderRandom = meal => {
     mealDiv.id = "random-meal-div"
     mealDiv.innerHTML =
         `<h1>${strMeal}</h1>
-                <h3>${strCategory} : ${strArea}</h3>
+                <h3>Origin: ${strArea} <br> Category: ${strCategory}</h3>
                 <a href=${strSource}>Full Recipe<a>
                 <p>${strInstructions}</p>
                 <img src=${strMealThumb}>`
     categoryProfile.append(mealDiv)
 }
 
+const renderAreaList = area => {
+    const {strArea} = area
+    const areaLi = document.createElement("li")
+    areaLi.className = "areaLi"
+    areaLi.id = `${strArea}`
+    areaLi.innerText = `${strArea}`
+
+//tryind to add meals to the DOM based on area----probably delete
+    areaLi.addEventListener("click", function () {
+        resetPage()
+        //fetchFilterArea(area)
+        const {strArea} = area
+        const areaDiv = document.createElement("div")
+        areaDiv.id = `${strArea}`
+        areaDiv.innerHTML = `${strArea}`
+        categoryProfile.append(areaDiv)
+    })
+//
+    categoryItem.append(areaLi)
+
+}
+
+
 
 //----------HELPER FUNCTIONS-----------------------------//
+
+
 const resetPage = () => {
     categoryItem.innerHTML = ""
     categoryProfile.innerHTML = ""
 }
 
+
+
 //--------- Attach EVENT LISTENERS-----------------------//
 
+//document.addEventListener('DOMContentLoaded', )
 categoriesAllBtn.addEventListener("click", fetchAllCategories)
-// areaAllBtn.addEventListener("click", fetchAllAreas)
+areaAllBtn.addEventListener("click", fetchAllAreas)
 // ingredientsAllBtn.addEventListener("click", fectchAllIngredients)
 randomAllBtn.addEventListener("click", fetchRandom)
+
+searchByname.addEventListener("submit", handleSearchNameResults)
+searchByFirstLetter.addEventListener('submit', handleSearchByFirstLetterResults)
+
+filterByIngredient.addEventListener('submit', handleFilterIngredientResults)
